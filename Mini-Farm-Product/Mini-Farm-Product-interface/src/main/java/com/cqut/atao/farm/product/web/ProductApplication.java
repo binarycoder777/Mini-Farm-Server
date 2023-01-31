b55d32a9bfd9b513b694ec9238377c1c6e99579a
atao <87337334@qq.com>
2023-01-31 17:48:35 +0800

    fix(Mini-Farm-Product):修复nacos拉取不到配置的问题

--- Mini-Farm-Product/Mini-Farm-Product-interface/src/main/java/com/cqut/atao/farm/product/web/ProductApplication.java
+++ Mini-Farm-Product/Mini-Farm-Product-interface/src/main/java/com/cqut/atao/farm/product/web/ProductApplication.java
@@ -1,6 +1,5 @@
 package com.cqut.atao.farm.product.web;
 
-import cn.hippo4j.core.enable.EnableDynamicThreadPool;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
