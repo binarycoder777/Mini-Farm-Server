5caefdd1bdc2af96c1419d4fb5d93bb75a23c4e6
atao <87337334@qq.com>
2023-03-15 14:57:14 +0800

    feat(Mini-Farm-Order):抽取订单接口，新增秒杀订单接口，避免在秒杀活动中，因共性的库存扣减等操作导致订单服务不可用

--- Mini-Farm-Order/Mini-Farm-Order-interface/src/main/java/com/cqut/atao/farm/order/web/controller/OrderController.java
+++ Mini-Farm-Order/Mini-Farm-Order-interface/src/main/java/com/cqut/atao/farm/order/web/controller/OrderController.java
@@ -1,6 +1,7 @@
 package com.cqut.atao.farm.order.web.controller;
 
 import com.cqut.atao.farm.order.application.process.OrderOperationProcess;
+import com.cqut.atao.farm.order.application.process.impI.KillOrderOperationProcessImpI;
 import com.cqut.atao.farm.order.domain.model.aggregate.Order;
 import com.cqut.atao.farm.order.domain.model.req.PlaceOrderReq;
 import com.cqut.atao.farm.springboot.starter.convention.result.Result;
@@ -32,8 +33,11 @@ public class OrderController {
     @Resource
     private OrderOperationProcess orderOperationProcess;
 
+    @Resource
+    private KillOrderOperationProcessImpI killOrderOperationProcessImpI;
+
     @PostMapping("/create")
-    @ApiOperation("产品下单")
+    @ApiOperation("商品下单")
     public Result<String> createOrder(@RequestBody PlaceOrderReq req) {
         String orderNo = orderOperationProcess.createOrder(req);
         return Results.success(orderNo);
@@ -46,6 +50,23 @@ public class OrderController {
     )
     public Result<Void> createOrder(@PathVariable String orderNo) {
         orderOperationProcess.cancelOrder(orderNo);
+        return Results.success();
+    }
+
+    @PostMapping("/kill/create")
+    @ApiOperation("秒杀商品下单")
+    public Result<String> createKillOrder(@RequestBody PlaceOrderReq req) {
+        String orderNo = killOrderOperationProcessImpI.createOrder(req);
+        return Results.success(orderNo);
+    }
+
+    @PostMapping("/kill/cancel/{orderNo}")
+    @ApiOperation("取消秒杀下单")
+    @ApiImplicitParams(
+            @ApiImplicitParam(name = "orderNo", value = "订单号", required = true, example = "1593868838284611584")
+    )
+    public Result<Void> cancelKillOrder(@PathVariable String orderNo) {
+        killOrderOperationProcessImpI.cancelOrder(orderNo);
         return Results.success();
     }
 
